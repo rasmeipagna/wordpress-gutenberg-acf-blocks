b94be3e1c9ff28a2f51b0a664a7603c2b19fc28b
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-05-31 15:44:34 +0200

    add sitemap

---
+++ template-parts/block/content-sitemap.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Texte en exergue block template
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'sitemap-' . $block['id'];
+if ( !empty($block['anchor']) ) {
+  $id = $block['anchor'];
+}
+
+$pages_exclues = get_field('pages_exclues');
+
+if ( !empty($pages_exclues) ) {
+  $exclude = implode($pages_exclues, ',');
+} else {
+  $exclude = '';
+}
+
+?>
+
+  <div id="<?php echo esc_attr($id); ?>" <?php acf_block_class($block, 'sitemap'); ?>>
+    <ul>
+      <?php wp_list_pages(array(
+        'exclude'  => $exclude,
+        'title_li' => null
+      )); ?>
+    </ul>
+  </div>
