9d3c3516450edcc81a17e65b56c1bbd7d4907815
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-06-03 10:59:50 +0200

    update css widget citation

--- template-parts/block/content-widget-citation.php
+++ template-parts/block/content-widget-citation.php
@@ -18,13 +18,13 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
     <div class="col-xs-12 col-sm-12 container-widget content">
         <div class="col-xs-12 mobile-bkg">
             <div class="col-sm-3">
-                    <div class="portrait"><img src="<?php echo $avatar['url']; ?>" alt="<?php echo $avatar['alt']; ?>" /><br />
-                    <h4><?php the_field('nom'); ?> <?php the_field('prenom'); ?></h4><br />
-                    <p class="promo"><strong><?php the_field('promo'); ?> <?php the_field('annee'); ?></strong></p>
+                    <div class="portrait"><img src="<?php echo $avatar['url']; ?>" alt="<?php echo $avatar['alt']; ?>" />
+                    <h4 class="citation"><?php the_field('nom'); ?> <?php the_field('prenom'); ?></h4>
+                    <p class="promo"><?php the_field('promo'); ?> <?php the_field('annee'); ?></p>
                     </div>
             </div>
             <div class="col-xs-12 col-sm-9 citation-content">
-            <p class="citation"><strong><?php the_field('citation'); ?></strong></p>       
+            <p class="citation"><?php the_field('citation'); ?></p>       
             </div>  
         </div>
              
