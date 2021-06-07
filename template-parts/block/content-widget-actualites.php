c652dc925abf9c59371a9c444b538c46e5fa8358
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-06-07 15:38:55 +0200

    update widget actu

--- template-parts/block/content-widget-actualites.php
+++ template-parts/block/content-widget-actualites.php
@@ -16,71 +16,8 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
         <div class="col-xs-12">
         <h4 class="titre-actualites"><?php the_field('titre'); ?></h4>
 
-            <div class="col-xs-12 col-sm-3">
-                    <div class="liste-metiers">
-                        <ul class="metier">
-                        <?php
-                            $featured_posts = get_field('liste_metiers');
-                            if( $featured_posts ): 
-                        ?>
-                            <ul>
-                        <?php foreach( $featured_posts as $featured_post ):
-                            $permalink = get_permalink( $featured_post->ID );
-                            $title = get_the_title( $featured_post->ID );
-                        ?>
-                                <li>
-                                <a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>
-                                </li>
-                        <?php endforeach; ?>
-                            </ul>
-                        <?php endif; ?>
-                        </ul>
-                    </div>
-            </div>
-            <div class="col-xs-12 col-sm-3">
-                <div class="pictos-domaines">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-6">
-                            <a href="<?php the_field('sante'); ?>">
-                            <img class="center domaines" src="<?php the_field('image_sante'); ?>" />
-                            </a>
-                            <a href="<?php the_field('energie'); ?>">
-                            <img class="center domaines" src="<?php the_field('image_energie'); ?>" />
-                            </a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-6">
-                            <a href="<?php the_field('cosmetique'); ?>">
-                            <img class="center domaines" src="<?php the_field('image_cosmetique'); ?>" />
-                            </a>
-                            <a href="<?php the_field('agriculture'); ?>">
-                            <img class="center domaines" src="<?php the_field('image_agriculture'); ?>" />
-                            </a>
-                        </div>
-                    </div>                   
-                </div>       
-            </div>
-            <div class="col-xs-12 col-sm-3">
-                    <div class="liste-metiers">
-                        <ul class="metier">
-                        <?php
-                            $featured_posts = get_field('liste_metiers');
-                            if( $featured_posts ): 
-                        ?>
-                            <ul>
-                        <?php foreach( $featured_posts as $featured_post ):
-                            $permalink = get_permalink( $featured_post->ID );
-                            $title = get_the_title( $featured_post->ID );
-                        ?>
-                                <li>
-                                <a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>
-                                </li>
-                        <?php endforeach; ?>
-                            </ul>
-                        <?php endif; ?>
-                        </ul>
-                    </div>
+            <div class="col-xs-12 ">
+                    
             </div>
             <div class="clearfix"></div>
             <div class="actualites-button">
