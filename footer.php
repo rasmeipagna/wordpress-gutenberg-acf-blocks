72e8f7b3ccd481a3d2366227e4d0c0bda74fd87d
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-06-09 18:49:20 +0200

    footer update

--- footer.php
+++ footer.php
@@ -38,7 +38,7 @@
                 
                 ?>
 
-<!-- appel le widget Footer  "à propos" dans le Footer -->
+<!-- appeler le widget Footer  "à propos" dans le Footer -->
                     <?php
                         if(is_active_sidebar('footer-1')) : ?>                            
                                 
@@ -105,13 +105,23 @@ wp_nav_menu( array(
 */
 ?>
                     </ul>
-					<div class="logo-reseaux">
+					<!--<div class="logo-reseaux">
                         <a class="facebook" href="https://www.facebook.com/supbiotech" target="_blank"></a>
                         <a class="twitter" href="https://twitter.com/supbiotech" target="_blank"></a>
                         <a class="youtube" href="https://www.youtube.com/user/supbiotech" target="_blank"></a>
                         <a class="insta" href="https://www.instagram.com/supbiotech/" target="_blank"></a>
                         <a class="linkedin" href="https://www.linkedin.com/edu/sup'biotech-%C3%A9cole-de-biotechnologies-43565" target="_blank"></a>
-                    </div>
+                    </div>-->
+                
+                    <!-- appel des réseaux sociaux -->
+                    <?php
+                        if(is_active_sidebar('social-media')) : ?>                            
+                                
+                                    <?php dynamic_sidebar('social-media'); ?>
+                                                            
+                        <?php endif; 
+                        ?>
+                   
                 </div>
             </div>
         </div><div class="clearfix"></div>
@@ -122,12 +132,9 @@ wp_nav_menu( array(
     $currentlang = get_bloginfo('language');
     if($currentlang=="en-GB") : ?>
         <!--<h3>Venez nous rencontrer</h3> -->
-			    <div class="col-md-6 rencontre">
-                &nbsp;							                
-                </div>
-
+			    
                 <div class="col-md-6 vousetes">
-                    <div class="inner">                   
+                    <div class="inner-en">                   
                         <a class="documentation" href="/engineering-school-admission/download-documentation">
                             Documentation
                         </a>
@@ -143,6 +150,10 @@ wp_nav_menu( array(
                     </div><!-- inner -->
                     <div class="clearfix"></div>
                 </div><!-- vous etes -->
+
+                <div class="col-md-6 rencontre">
+                &nbsp;							                
+                </div>
     
     <?php else : ?> 
 
@@ -214,11 +225,11 @@ wp_nav_menu( array(
         <?php
     $currentlang = get_bloginfo('language');
     if($currentlang=="en-GB") : ?>
-        Privately Endowed Graduate Educational Institution - Education Authority of Créteil Registration - This school is a member of <a class="firstLineFooter" href="http://www.ionis-group.com" rel="nofollow" title="Privately Endowed Graduate Educational Institution" target="_blank">IONIS Education Group</a> such as :<br><br>
+        <p>Privately Endowed Graduate Educational Institution - Education Authority of Créteil Registration - This school is a member of <a class="firstLineFooter" href="http://www.ionis-group.com" rel="nofollow" title="Privately Endowed Graduate Educational Institution" target="_blank">IONIS Education Group</a> such as :</p>
             
     
     <?php else : ?>   
-        Établissement privé d'enseignement supérieur technique - Inscription au rectorat de Créteil - IPSA est membre de <a class="firstLineFooter" href="http://www.ionis-group.com" rel="nofollow" title="enseignement supérieur privé" target="_blank">IONIS Education Group</a> comme :<br><br>
+        <p>Établissement privé d'enseignement supérieur technique - Inscription au rectorat de Créteil - IPSA est membre de <a class="firstLineFooter" href="http://www.ionis-group.com" rel="nofollow" title="enseignement supérieur privé" target="_blank">IONIS Education Group</a> comme :</p>
             <!--<ul class=""><li><a href="http://www.isg.fr" title="ecole commerce paris" target="_blank">ISG</a> - </li><li><a href="http://www.modadomani.fr" title="business school" target="_blank">Moda Domani Institute</a> - </li><li><a href="http://bfs.iseg.fr" title="ecole de commerce" target="_blank">ISEG Business &amp; Finance School</a> - </li><li><a href="http://mcs.iseg.fr" title="ecole de communication, ecole de marketing, ecole de publicite" target="_blank">ISEG Marketing &amp; Communication School</a> - </li><li><a href="http://www.isth.fr" title="prepa sciences po hec" target="_blank">ISTH</a> - </li><li><a href="http://www.ics-begue.com" title="école expert finance" target="_blank">ICS Bégué</a> - </li><li><a href="http://www.initial-isefac.com" title="bachelor commerce communication" target="_blank">ISEFAC Bachelor</a> - </li><li><a href="http://www.isefac-rh.fr" title="" target="_blank">ISEFAC R.H.</a></li></ul><ul class=""><li><a href="http://www.epita.fr" title="école ingénieur informatique" target="_blank">EPITA</a> - </li><li><a href="http://www.esme.fr" title="ecole ingenieur" target="_blank">ESME Sudria</a> - </li><li><a href="http://www.epitech.eu" title="ecole informatique" target="_blank">Epitech</a> - </li><li><a href="http://webacademie.org/" title="" target="_blank">Web@cadémie</a> - </li><li><a href="http://www.supbiotech.fr" title="ecole biotechnologie" target="_blank">Sup'Biotech</a> - </li><li><a href="http://www.e-artsup.net" title="ecole multimedia" target="_blank">e-artsup</a> - </li><li><a href="http://www.ionis-stm.com" title="mastere post master management finance informatique" target="_blank">Ionis-STM</a> - </li><li><a href="http://www.supinternet.fr" title="ecole internet" target="_blank">SUP'Internet</a></li></ul><ul class=""><li><a href="http://www.isefac-alternance.fr" title="bts alternance paris" target="_blank">ISEFAC Alternance</a> - </li><li><a href="http://www.etna-alternance.net" title="informatique alternance" target="_blank">ETNA</a> - </li><li><a href="http://www.ionis-up.fr" title="" target="_blank">IONIS UP</a> - </li><li><a href="http://www.coding-academy.fr" title="" target="_blank">Coding Academy</a> - </li><li><a href="http://www.ionisx.com" title="" target="_blank">IONISx</a> - </li><li><a href="http://www.ionis361.com" title="" target="_blank">IONIS 361</a></li></ul>-->    
         <?php endif; ?>
             
