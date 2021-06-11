abdb4e64bab315474e03a9a2d7eca145a8c4c835
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-06-11 16:50:49 +0200

    update replay slider

--- functions.php
+++ functions.php
@@ -360,19 +360,19 @@ function my_acf_block_render_callback( $block ) {
 
 if( function_exists('acf_add_options_page') ) {
 	
-	// acf_add_options_page(array(
-	// 	'page_title' 	=> 'Theme General Settings',
-	// 	'menu_title'	=> 'Theme Settings',
-	// 	'menu_slug' 	=> 'theme-general-settings',
-	// 	'capability'	=> 'edit_posts',
-	// 	'redirect'		=> false
-	// ));
+	acf_add_options_page(array(
+	 	'page_title' 	=> 'Theme General Settings',
+	 	'menu_title'	=> 'Theme Settings',
+	 	'menu_slug' 	=> 'theme-general-settings',
+	 	'capability'	=> 'edit_posts',
+	 	'redirect'		=> false
+	 ));
 	
-	// acf_add_options_sub_page(array(
-	// 	'page_title' 	=> 'Theme Header Settings',
-	// 	'menu_title'	=> 'Header',
-	// 	'parent_slug'	=> 'theme-general-settings',
-	// ));
+	 acf_add_options_sub_page(array(
+	 	'page_title' 	=> 'slider replay',
+	 	'menu_title'	=> 'Replay',
+	 	'parent_slug'	=> 'theme-general-settings',
+	 ));
 	
 	/*acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Footer Settings',
