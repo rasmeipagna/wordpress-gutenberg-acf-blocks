b1f11f1374c8d217c651fe273689f37872ecce66
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-06-18 18:10:20 +0200

    udpate widget citation

--- template-parts/block/content-widget-citation.php
+++ template-parts/block/content-widget-citation.php
@@ -17,18 +17,17 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
 <div id="<?php echo $id; ?>" class="widget-citation <?php echo $align_class; ?>">
     <div class="col-xs-12 col-sm-12 container-widget content">
         <div class="col-xs-12 mobile-bkg">
-            <div class="col-sm-3">
-                    <div class="portrait"><img src="<?php echo $avatar['url']; ?>" alt="<?php echo $avatar['alt']; ?>" />
-                    
+            <div class="col-sm-2">
+                    <div class="portrait">
+                        <img src="<?php echo $avatar['url']; ?>" alt="<?php echo $avatar['alt']; ?>" />  
+                        <div class="clearfix"></div>
+                        <span class="citation"><?php the_field('nom'); ?> <?php the_field('prenom'); ?></span><br />
+                        <span class="promo"><?php the_field('promotion'); ?></span>                  
                     </div>
+                    
             </div>
-            <div class="col-xs-12 col-sm-9 citation-content">
-            <p class="citation"><?php the_field('citation'); ?>
-                <div class="position-citation-promo">
-                    <span class="citation"><?php the_field('nom'); ?> <?php the_field('prenom'); ?></span><br />
-                    <span class="promo"><?php the_field('promotion'); ?></span>
-                </div>
-            </p>  
+            <div class="col-xs-12 col-sm-10 citation-content">
+            <p class="citation"><?php the_field('citation'); ?></p>  
             </div>  
         </div>
              
